67bcab9c5beedfebd6a65d3c2689d5c67c9f39b3
Licolnlee <32893599+Licolnlee@users.noreply.github.com>
2020-05-11 09:11:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,11 +14,11 @@ sed -i 's/192.168.1.1/192.168.123.156/g' package/base-files/files/bin/config_gen
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-autoipsetadder package/luci-app-autoipsetadder
-git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
-git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
+#git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 git clone https://github.com/Licolnlee/Feed-Licolnlee package/Feed-Licolnlee
-git clone https://github.com/fw876/helloworld package/helloworld
-git clone https://github.com/Licolnlee/passwall packages/passwall
-git clone https://github.com/kenzok8/small package/small
+#git clone https://github.com/fw876/helloworld package/helloworld
+#git clone https://github.com/Licolnlee/passwall packages/passwall
+#git clone https://github.com/kenzok8/small package/small
 # git clone https://github.com/pexcn/openwrt-chinadns-ng package/chinadns-ng
 # git clone https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng package/feeds-xiaoqingfeng
